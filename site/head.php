3f1fbd528e8e0127ad2be014ba0cb7db3fb763b9
fericci <felipe.nascimento@seepix.com.br>
2017-06-05 16:12:16 -0300

    substitiundo semantic por materialize

--- site/head.php
+++ site/head.php
@@ -2,11 +2,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Gótica Suave</title>
-	<script type="text/javascript" src="semantic/jquery.min.js"></script>
-	
-	<script type="text/javascript" src="semantic/semantic.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
-	
+
+
+	<link rel="stylesheet" href="materialize/css/materialize.min.css">
 	<link rel="stylesheet" href="css/estilo.css">
-	<link rel="stylesheet" href="semantic/semantic.css">
 </head>
